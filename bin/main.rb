ed3f713f780246010de8cc0c90e4c2e84115d2af
Tendongze Godson <tendongze95@gmail.com>
2020-06-17 10:31:27 +0100

    create a board map and user promp message

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,20 @@
 #!/usr/bin/env ruby
 
 puts "Hello, world!"
+
+puts"     A   B   C
+    ===============
+1  | A1 | B1 | C1 |
+    ---  ---- ----
+2  | A2 | B2 | C2 |
+    ---- ---- ----
+3  | A3 | B3 | C3 |
+    ---- ---- ---
+    
+    player 1 use x
+    player 2 use o"
+
+puts "player 1 please make your move!"
+player_1_input=gets.chomp 
+puts player_1_input
+ 
