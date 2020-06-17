28b8cfd5d33815c6bf9487b78bb17595ebc7beb8
Abdullah Numan <anewman15@hotmail.com>
2020-06-17 16:29:09 +0600

    Add more possible user prompts and user input options

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
-puts "Hello, world!"
+puts 'Hello, world!'
 
-puts"     A   B   C
+puts "      A    B    C
     ===============
 1  | A1 | B1 | C1 |
     ---  ---- ----
@@ -10,11 +10,57 @@ puts"     A   B   C
     ---- ---- ----
 3  | A3 | B3 | C3 |
     ---- ---- ---
-    
-    player 1 use x
-    player 2 use o"
 
-puts "player 1 please make your move!"
-player_1_input=gets.chomp 
-puts player_1_input
- 
+    Player 1: use X
+    Player 2: use O
+
+    "
+
+# First turn for Player 1
+puts 'Player 1, please make your move!'
+player1_input = gets.chomp
+
+puts "Are you sure you want to put X into #{player1_input} - Y/N?"
+player1_decision = gets.chomp
+
+# if Yes
+puts 'Your move is displayed on the board.'
+# Display updated board
+
+# if No
+puts 'Player 1, please make your move!'
+player1_input = gets.chomp
+
+puts "Are you sure you want to put X into #{player1_input} - Y/N?"
+player1_decision = gets.chomp
+
+# Turn for player 2
+puts "Now it's Player 2's turn. Player 2, please make your move"
+player2_input = gets.chomp
+
+puts "Are you sure you want to put O into #{player2_input} - Y/N?"
+player1_decision = gets.chomp
+
+# if Yes
+puts 'Your move is displayed on the board.'
+# Display updated board
+
+# if No
+puts 'Player 2, please make your move!'
+player2_input = gets.chomp
+
+puts "Are you sure you want to put O into #{player2_input} - Y/N?"
+player2_decision = gets.chomp
+
+# New turn for player 1
+puts "Now it's Player 1's turn. Player 1, please make your move"
+player1_input = gets.chomp
+
+# if user input already occupied
+puts "#{player1_input} is already occupied. Please look at the board and fill another cell."
+# Display board
+
+# if 3 contiguous cells are occupied by the same player
+puts 'Player 1 won the game!'
+# OR
+puts 'Player 2 won the game!'
