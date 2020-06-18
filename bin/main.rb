d791d85e0b9fad180cf86ffe2d8a3e2d9adf7b7f
Abdullah Numan <anewman15@hotmail.com>
2020-06-18 18:26:37 +0600

    Change board cell indicators

--- bin/main.rb
+++ bin/main.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 
-puts 'Hello, world!'
+puts 'Welcome to your favorite schooldays game!'
 
-puts "      A    B    C
-    ===============
-1  | A1 | B1 | C1 |
-    ---  ---- ----
-2  | A2 | B2 | C2 |
-    ---- ---- ----
-3  | A3 | B3 | C3 |
-    ---- ---- ---
+puts "
+   ===========
+  | 1 | 2 | 3 |
+   --- --- ---
+  | 4 | 5 | 6 |
+   --- --- ---
+  | 7 | 8 | 9 |
+   ===========
 
 Player 1: use X
 Player 2: use O
@@ -24,9 +24,18 @@ puts 'Please enter the name of Player 2'
 player2_name = gets.chomp
 puts "player 2 is called #{player2_name}"
 
-available_moves = %w[A1 B1 C1 A2 B2 C2 A3 B3 C3]
-WINNING_COMBINATIONS = [%w[A1 B1 C1], %w[A2 B2 C2], %w[A3 B3 C3], %w[A1 A2 A3], %w[B1 B2 B3],
-                        %w[C1 C2 C3], %w[A1 B2 C3], %w[C1 B2 A3]].freeze
+available_moves = %w[1 2 3 4 5 6 7 8 9]
+WINNING_COMBINATIONS = [
+                        %w[1 2 3],
+                        %w[4 5 6],
+                        %w[7 8 9],
+                        %w[1 4 7],
+                        %w[2 5 8],
+                        %w[3 6 8],
+                        %w[1 5 9],
+                        %w[3 5 7]
+                      ].freeze
+                      
 player1_moves = 0
 player2_moves = 0
 winning_move = false
